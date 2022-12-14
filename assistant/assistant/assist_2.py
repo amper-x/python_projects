da16d0c18a7beb43a5f316558d7535ac1f8633f9
amper-x <114496460+amper-x@users.noreply.github.com>
2022-12-14 20:49:05 +0200

    hw 11

--- assistant/assistant/assist_2.py
+++ assistant/assistant/assist_2.py
@@ -20,15 +20,19 @@ def parser(command: list):  # Парсер команд. Видає відпов
     execution = {
         "create": create_contact,
         "delete contact": delete_contact,
-        "add": add_number,
-        "change": change_number,
+        "add number": add_number,
+        "change number": change_number,
         "delete phone": delete_phone,
         "show phones": show_phones,
         "show all": show_all,
         "close": end_script,
         "exit": end_script,
         "good bye": end_script,
-        "hello": greet_again
+        "hello": greet_again,
+        "add birthday": add_birthday,
+        "birthday": show_birthday,
+        "next birthday": days_to_birthday,
+        "show part": show_part
     }
     tmp = " ".join(command[0:2])
     if tmp in execution.keys():
@@ -59,28 +63,22 @@ def delete_contact(command: list) -> None:
 
 @input_error
 def add_number(command: list) -> None:
-    name = command[1:-1]
+    name = command[2:-1]
     for count, value in enumerate(name):
         name[count] = value.lower().capitalize()
     name = " ".join(name)
     number = command[-1]
-    if number.isdecimal():
-        book.data[name].add_phone(number)
-    else:
-        print("Please enter a valid phone number (only decimals; no whitespaces).")
+    book.data[name].add_phone(number)
 
 
 @input_error
 def change_number(command: list) -> None:
-    name = command[1:-2]
+    name = command[2:-2]
     for count, value in enumerate(name):
         name[count] = value.lower().capitalize()
     name = " ".join(name)
     old, new = command[-2], command[-1]
-    if old.isdecimal() and new.isdecimal():
-        book.data[name].change_phone(old, new)
-    else:
-        print("Please enter 2 valid phone numbers.")
+    book.data[name].change_phone(old, new)
 
 
 @input_error
@@ -103,9 +101,55 @@ def show_phones(command: list) -> None:
 
 
 @input_error
+def add_birthday(command: list) -> None:
+    name = command[2:-1]
+    for count, value in enumerate(name):
+        name[count] = value.lower().capitalize()
+    name = " ".join(name)
+    if not book.data[name].birthday.value:
+        book.data[name].add_birthday(command[-1])
+    else:
+        print("This contact already has a birthday.")
+
+
+@input_error
+def show_birthday(command: list) -> None:
+    name = command[1:]
+    for count, value in enumerate(name):
+        name[count] = value.lower().capitalize()
+    name = " ".join(name)
+    if not book.data[name].birthday.value:
+        print("This contact doesn't have a birthday.")
+    else:
+        print(book.data[name].birthday.value)
+
+
+@input_error
+def days_to_birthday(command: list) -> None:
+    name = command[2:]
+    for count, value in enumerate(name):
+        name[count] = value.lower().capitalize()
+    name = " ".join(name)
+    print(f"Days to next birthday of {name} in {book.data[name].days_to_birthday()} days.")
+
+
+@input_error
 def show_all(command: list) -> None:
     for key, val in book.data.items():
-        print(f"Name: {key}\nPhones: {[i.value for i in val.phones]}\nEmails: {[i.value for i in val.emails]}")
+        print(f"Name: {key}\nBirthday: {val.birthday}\nPhones: {[i.value for i in val.phones]}"
+              f"\nEmails: {[i.value for i in val.emails]}")
+
+
+@input_error
+def show_part(command: list) -> None:
+    n = command[-1]
+    counter = 1
+    for chunk in book.iterator(int(n)):
+        print(f"\nChunk {counter}:\n")
+        counter += 1
+        for record in chunk:
+            print(f"Name: {record.name.value}\nBirthday: {record.birthday}\nPhones: {[i.value for i in record.phones]}"
+                  f"\nEmails: {[i.value for i in record.emails]}")
 
 
 @input_error
@@ -136,7 +180,6 @@ def main():  # Основна логіка програми
         command = command.split()
         if parser(command)(command):
             break
-    print(book)
 
 
 if __name__ == "__main__":
--- assistant/assistant/classes.py
+++ assistant/assistant/classes.py
@@ -1,4 +1,6 @@
 from collections import UserDict
+import datetime
+import re
 
 
 class AddressBook(UserDict):
@@ -15,22 +17,35 @@ class AddressBook(UserDict):
     def add_record(self, name):
         self.data[name] = Record(name)
 
+    def iterator(self, n=1):
+        output = []
+        i = 0
+
+        for elem in self.data.values():
+            output.append(elem)
+            i += 1
+            if i == n:
+                yield output
+                output = []
+                i = 0
+        if output:
+            yield output
+
 
 class Record:
     """
     Об'єкт, що зберігає в собі всю інформацію про контакт. При декларації приймає обов'язковий клас 'Name'.
     Має методи для додавання та редагування необов'язкового атрибута 'Phone'.
     """
-    def __init__(self, name, phone=None, email=None):
+    def __init__(self, name, phone=None, birthday=None, email=None):
         self.name = Name(name)
         self.phones = [Phone(phone)] if phone else []
         self.emails = [Email(email)] if email else []
+        self.birthday = Birthday(birthday)
 
     def add_phone(self, phone):  # Додати новий телефон
-        for i in self.phones:
-            if phone == i.value:
-                return
-        self.phones.append(Phone(phone))
+        if phone not in list(map(lambda x: x.value, self.phones)) and Phone(phone).value:
+            self.phones.append(Phone(phone))
 
     def del_phone(self, phone):  # Видалити існуючий телефон
         for i in self.phones:
@@ -42,6 +57,18 @@ class Record:
             if phone == i.value:
                 self.phones.remove(i)
                 self.phones.append(Phone(new_phone))
+
+    def add_birthday(self, new_birthday):
+        self.birthday = Birthday(new_birthday)
+
+    def days_to_birthday(self):
+        today = datetime.date.today()
+        if not self.birthday.value:
+            return None
+        delta = datetime.date(today.year, self.birthday.value.month, self.birthday.value.day) - today
+        if delta.days < 0:
+            delta = datetime.date(today.year + 1, self.birthday.value.month, self.birthday.value.day) - today
+        return delta.days
 
     def add_email(self, email):  # Поки не реалізований функціонал
         for i in self.emails:
@@ -71,7 +98,48 @@ class Name(Field):  # Поки функціоналу батьківського
 
 
 class Phone(Field):
-    pass
+    def __init__(self, value):
+        super().__init__(value)
+        self.__value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        if new_value.isdecimal():
+            self.__value = new_value
+
+
+class Birthday(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.__value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            return
+        tmp = re.findall(r"(?<![1-9])\d{1,4}", new_value)
+        try:
+            self.__value = datetime.date(int(tmp[2]), int(tmp[1]), int(tmp[0]))
+        except IndexError:
+            print("Please enter a valid date format: day:month:year, day.month.year, etc.")
+        except ValueError:
+            print("Such a date doesn't exist.")
+
+    def __repr__(self):
+        if self.__value is None:
+            return "No birthday recorded."
+        else:
+            return f"Day: {self.__value.day}, Month: {self.__value.month}, Year: {self.__value.year}"
 
 
 class Email(Field):
